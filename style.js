e828d7f390bbd4b70955684045a7c5eb65f55f8b
XAVAX ​ <mdavaf17@gmail.com>
2022-11-18 17:32:24 +0700

    fix update selection choice

--- style.js
+++ style.js
@@ -1,9 +1,7 @@
-// Selecting elements.
 const pScore = document.getElementById('playerScore');
 const cScore = document.getElementById('computerScore');
 const buttons = document.querySelectorAll('.selection button');
 
-// Storing the scores.
 let computerScore = localStorage.getItem("cscore");
 let playerScore = localStorage.getItem("pscore");
 
@@ -36,39 +34,57 @@ const lose = ()=>{
     computerScore++;
 }
 
-// Game Functionality.
 function game(userchoice){
     let randomNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
     console.log(randomNum);
     compchoice = choices[randomNum];
 
-    // If it's a Tie .
+    if (userchoice == "rock"){
+        document.getElementById("userchoice").className = "fa-solid fa-hand-back-fist";
+    }
+    if (userchoice == "paper"){
+        document.getElementById("userchoice").className = "fa-solid fa-hand";
+    }
+    if (userchoice == "scissor"){
+        document.getElementById("userchoice").className = "fa-solid fa-hand-scissors";
+    }
+
+    if (compchoice == "rock"){
+        document.getElementById("compchoice").className = "fa-solid fa-hand-back-fist";
+    }
+    if (compchoice == "paper"){
+        document.getElementById("compchoice").className = "fa-solid fa-hand";
+    }
+    if (compchoice == "scissor"){
+        document.getElementById("compchoice").className = "fa-solid fa-hand-scissors";
+    }
+
+
+
     if(userchoice === compchoice){
         pScore.innerHTML = pScore.innerHTML;
         cScore.innerHTML = cScore.innerHTML;
         tie();
     }
-
-        // if it's not a Tie.
-        else if(userchoice === "rock" && compchoice === "scissor"){
-            win();
-            pScore.innerHTML = playerScore;
-        }else if(userchoice === "rock" && compchoice === "paper"){
-            lose();
-            cScore.innerHTML = computerScore;
-        }else if(userchoice === "paper" && compchoice === "scissor"){
-            lose();
-            cScore.innerHTML = computerScore;
-        }else if(userchoice === "paper" && compchoice === "rock"){
-            win();
-            pScore.innerHTML = playerScore;
-        }else if(userchoice === "scissor" && compchoice === "rock"){
-            lose();
-            cScore.innerHTML = computerScore;
-        }else if(userchoice === "scissor" && compchoice === "paper"){
-            win();
-            pScore.innerHTML = playerScore;
-        }
+    else if(userchoice === "rock" && compchoice === "scissor"){
+        win();
+        pScore.innerHTML = playerScore;
+    }else if(userchoice === "rock" && compchoice === "paper"){
+        lose();
+        cScore.innerHTML = computerScore;
+    }else if(userchoice === "paper" && compchoice === "scissor"){
+        lose();
+        cScore.innerHTML = computerScore;
+    }else if(userchoice === "paper" && compchoice === "rock"){
+        win();
+        pScore.innerHTML = playerScore;
+    }else if(userchoice === "scissor" && compchoice === "rock"){
+        lose();
+        cScore.innerHTML = computerScore;
+    }else if(userchoice === "scissor" && compchoice === "paper"){
+        win();
+        pScore.innerHTML = playerScore;
+    }
     localStorage.setItem("pscore", playerScore);
     localStorage.setItem("cscore", computerScore);
 }
